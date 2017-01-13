99bca675b58d811acf84ecc25e6e9d995c8b88cf
LiuShichang <shichang.liu@hunteron.com>
2017-01-13 18:48:46 +0800

    Update Code

--- src/main/java/lsc/disruptor/performanceCompare/DisruptorTest.java
+++ src/main/java/lsc/disruptor/performanceCompare/DisruptorTest.java
@@ -6,18 +6,15 @@ import java.util.concurrent.Executors;
 import lsc.disruptor.performanceCompare.bean.LogEvent;
 import lsc.disruptor.performanceCompare.bean.LogEventFactory;
 
-import com.lmax.disruptor.BusySpinWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
-import com.lmax.disruptor.dsl.ProducerType;
-import com.lmax.disruptor.util.DaemonThreadFactory;
 
 public class DisruptorTest {
     public static void main(String[] args) {
         LogEventFactory factory = new LogEventFactory();
         int ringBufferSize = 1024*1024;
         Executor executor=Executors.newCachedThreadPool();
-        final Disruptor<LogEvent> disruptor = new Disruptor<LogEvent>(factory, ringBufferSize, executor, DaemonThreadFactory.INSTANCE, ProducerType.SINGLE);
+        final Disruptor<LogEvent> disruptor = null;//new Disruptor<LogEvent>(factory, ringBufferSize, executor, DaemonThreadFactory.INSTANCE, ProducerType.SINGLE);
         LogEventConsumer consumer = new LogEventConsumer();
         disruptor.handleEventsWith(consumer);
         disruptor.start();
